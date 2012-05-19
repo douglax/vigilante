d5cf9ec4bf27e5548892ed3d88d95b9349e65e1a
Alejandro Acosta <alxacostaa@gmail.com>
2012-05-18 22:26:37 -0600

    genesis

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vigilante::Application.config.secret_token = '06fb93ccd19d330e70a38df7ced69a51e38011a01b39fbdaf453ff3ba45e44460ba61922988be0299c90a827f1b081f31b76913a776b9082d38d8faf37234fc7'
