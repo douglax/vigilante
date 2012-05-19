76e8b0593ad87ddb0d49a4ecf869e7e03950df04
Ivan Chavero <ichavero@aumenta.com.mx>
2012-05-19 02:43:23 -0600

    Added controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Vigilante::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :reports
 
   # Sample resource route with options:
   #   resources :products do
